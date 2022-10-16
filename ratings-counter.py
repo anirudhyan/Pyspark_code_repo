f514d83650a77e56d573d90fc7a4c4fce700db79
anirudhyan <anirudhya0707@gmail.com>
2022-10-16 21:03:43 +0530

    Spark_learning commit

--- ratings-counter.py
+++ ratings-counter.py
@@ -6,8 +6,9 @@ sc = SparkContext(conf = conf)
 
 lines = sc.textFile("file:///Spark_Course/ml-100k/u.data")
 ratings = lines.map(lambda x: x.split()[2])  # creates a new rdd and this is transformation
-result = ratings.countByValue()               # this is an action and it returns a normal python object 
+result = ratings.countByValue()               # this is an action and it returns a normal python object {dictionary}
 print(type(result))
+
 
 sortedResults = collections.OrderedDict(sorted(result.items()))
 print(type(sortedResults))
